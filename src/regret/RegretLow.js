6d5a9fa46245a0093cc292c1ff4f6cb95f1d216e
sjsr03 <sjsr03@hanmail.net>
2019-06-06 19:13:22 +0900

    충동구매 리스트 추가

---
+++ src/regret/RegretLow.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from "@material-ui/core/Button";
+import HomeButton from "@material-ui/icons/Home";
+import { Link } from "react-router-dom";
+import { getLowList} from '../storage/regret';
+
+export default function RegretLow() {
+  const lowList = getLowList();
+
+  return (
+    <div>
+      <Link to={"/"}>
+        <Button>
+          <HomeButton/>
+        </Button>
+      </Link>
+      <h1 align="center">Regret : Low</h1>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>카테고리 명</TableCell>
+            <TableCell >금액(원)</TableCell>
+            <TableCell >regret</TableCell>
+            <TableCell >날짜(일)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {lowList.map(({category, amount, regret, day}, i) => (
+          <TableRow key={i}>
+            <TableCell component="th" scope="row">
+              {category}
+            </TableCell>
+            <TableCell >{amount}</TableCell>
+            <TableCell>{regret}</TableCell>
+            <TableCell>{day}</TableCell>
+          </TableRow>
+        ))}
+        </TableBody>
+      </Table>
+
+    </div>
+  )
+}
